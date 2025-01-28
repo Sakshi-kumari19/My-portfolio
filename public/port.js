7a90499d36bad752edc76f6298e96628544dd2ec
chandan kumar <chandankumar700451@gamil.com>
2025-01-28 12:43:54 +0530

    rearrange the directory

---
+++ public/port.js
@@ -0,0 +1,8 @@
+
+
+    var typed = new Typed('.auto-type', {
+        strings: ["Frontend Developer", "Backend Developer", "Web Developer"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
